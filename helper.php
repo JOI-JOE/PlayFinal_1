0b6513273592679d9e882836a7e5b4e8de89bc5b
JOI-JOE <iknowhtml161@email.com>
2024-06-03 23:03:10 +0700

    add helper

--- helper.php
+++ helper.php
@@ -1,7 +1,16 @@
 <?php
 
+const PATH_ROOT = __DIR__ . '/';
+
 if (!function_exists('asset')) {
     function asset($path)
+    {
+        return $_ENV['BASE_URL'] . $path;
+    }
+}
+
+if (!function_exists('url')) {
+    function url($path)
     {
         return $_ENV['BASE_URL'] . $path;
     }
